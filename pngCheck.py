22bca5c40af44cfd80b3e17755ccdb4ff3717810
Whiffe <33534050+Whiffe@users.noreply.github.com>
2023-03-17 09:36:09 +0800

    Update pngCheck.py

--- pngCheck.py
+++ pngCheck.py
@@ -1,1 +1,16 @@
-# pngCheck.py 是检查截图是不是png格式
+# pngCheck.py 的作用是检查via文件中图片格式是否正确
+# python pngCheck.py --riseHand_via_dataset ./riseHand_via_dataset
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--riseHand_via_dataset', default='./riseHand_via_dataset',type=str)
+arg = parser.parse_args()
+riseHand_Dataset = arg.riseHand_via_dataset
+
+# 循环 读出所有json文件            
+for root, dirs, files in os.walk(riseHand_Dataset, topdown=False):
+    for name in files:
+        if not '.png' in name :
+            if not '.json' in name:
+                print(os.path.join(root, name))
